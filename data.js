0a1e4671455539ed575743704a079b4b554c4a20
EvertNL-dev <evertnlinsta@gmail.com>
2026-07-21 15:13:41 +0200

    Add default data structure to data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+window.CLEAN_DEFAULT_DATA = {
+  user: { name: 'Evert', initials: 'E' },
+  categories: [
+    {
+      name: 'Frozen Cleansift',
+      products: [
+        {
+          title: 'Emergency',
+          description: 'Korte omschrijving',
+          image: 'https://images.unsplash.com/photo-1506617420156-8e4536971650?auto=format&fit=crop&w=900&q=80',
+          brand: 'TopOrganic',
+          variants: [
+            { amount: '10 gram', price: '25' },
+            { amount: '20 gram', price: '45' },
+            { amount: '50 gram', price: '100' }
+          ]
+        }
+      ]
+    }
+  ]
+};
